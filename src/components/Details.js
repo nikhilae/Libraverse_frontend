062da5b5bcb96541af10bfafe02df44b77125b4a
Shreyas <logsinx96@gmail.com>
2024-07-15 21:56:13 +0530

    added quantity dropdown and book total

--- src/components/Details.js
+++ src/components/Details.js
@@ -5,14 +5,29 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
+import {InputLabel, Select, MenuItem} from "@mui/material";
+import { useState } from "react";
+import axios from "axios";
 // function Details() {
 //   const a = useLocation();
 //   console.log();
 
 function Details() {
   const a = useLocation().state;
+  var [quantity, setQuantity] = useState(1);
 
   console.log(a);
+
+  function addToCartClickFun(){
+    console.log("Add to cart button clicked!!")
+    // Backend call 
+    // var response = axios.get("localhost:8080/addToCart");
+    // const obj = [obj.title, obj.imageUrl, obj.quantity, obj.price];
+    // pOST request 
+
+  }
+
+
   return (
     <div>
       {
@@ -31,12 +46,43 @@ function Details() {
               <Typography variant="body2" color="text.secondary">
                 {a.description}
               </Typography>
-              <Typography variant="body2" color="text.secondary">
-                ${a.price}
+              <Typography variant="body3" color="text.primary">
+                Price: ${a.price}
+              </Typography>
+              <Typography>
+              {/* <InputLabel id="demo-simple-select-label">Age</InputLabel> */}
+  Quantity &nbsp;<Select
+    id="demo-simple-select"
+    value={quantity}
+    onChange={function(event){
+        // here in this function
+        // i have to set value field above
+        // to the new changed value
+        // in event.target.value
+        setQuantity(event.target.value);
+    }}
+    // when i change dropdown value
+    // onChange event will be triggered
+    // i have to set value/quantity to this new event's value
+  >
+    <MenuItem value={1}>1</MenuItem>
+    <MenuItem value={2}>2</MenuItem>
+    <MenuItem value={3}>3</MenuItem>
+    <MenuItem value={4}>4</MenuItem>
+    <MenuItem value={5}>5</MenuItem>
+  </Select>
+  {/* Quantity selected: {quantity} */}
+  {/* <br /> */}
+  &nbsp;
+  Book Total: ${a.price * quantity}
               </Typography>
             </CardContent>
           </CardActionArea>
           <CardActions>
+          <Button onClick={addToCartClickFun()} size="small" color="primary">
+              Add to cart
+              {/* quantity, price, title, imageURL */}
+            </Button>
             <Button size="small" color="primary">
               Buy now
             </Button>
