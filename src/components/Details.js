c49a32dde1d8ed766ed650d2dea2ca192a9320e2
E Nikhila <61705854+nikhilae@users.noreply.github.com>
2024-07-16 14:32:53 -0400

    cart entity additon

--- src/components/Details.js
+++ src/components/Details.js
@@ -5,7 +5,7 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
-import {InputLabel, Select, MenuItem} from "@mui/material";
+import { InputLabel, Select, MenuItem } from "@mui/material";
 import { useState } from "react";
 import axios from "axios";
 // function Details() {
@@ -18,21 +18,25 @@ function Details() {
 
   console.log(a);
 
-  function addToCartClickFun(){
-    console.log("Add to cart button clicked!!")
-    // Backend call 
-    var body={
-      title: a.title, imageUrl: a.imageUrl, quantity: quantity, price: a.price
-    } 
-    
-    var response = axios.post("http://localhost:8080/cart/addToCart", body);
+  async function addToCartClickFun() {
+    console.log("Add to cart button clicked!!");
+    // Backend call
+    var body = {
+      title: a.title,
+      imageUrl: a.imageUrl,
+      quantity: quantity,
+      price: a.price,
+    };
+
+    var response = await axios.post(
+      "http://localhost:8080/cart/addToCart",
+      body
+    );
 
     console.log(response);
     // const obj = [obj.title, obj.imageUrl, obj.quantity, obj.price];
-    // pOST request 
-
+    // pOST request
   }
-
 
   return (
     <div>
@@ -56,36 +60,36 @@ function Details() {
                 Price: ${a.price}
               </Typography>
               <Typography>
-              {/* <InputLabel id="demo-simple-select-label">Age</InputLabel> */}
-  Quantity &nbsp;<Select
-    id="demo-simple-select"
-    value={quantity}
-    onChange={function(event){
-        // here in this function
-        // i have to set value field above
-        // to the new changed value
-        // in event.target.value
-        setQuantity(event.target.value);
-    }}
-    // when i change dropdown value
-    // onChange event will be triggered
-    // i have to set value/quantity to this new event's value
-  >
-    <MenuItem value={1}>1</MenuItem>
-    <MenuItem value={2}>2</MenuItem>
-    <MenuItem value={3}>3</MenuItem>
-    <MenuItem value={4}>4</MenuItem>
-    <MenuItem value={5}>5</MenuItem>
-  </Select>
-  {/* Quantity selected: {quantity} */}
-  {/* <br /> */}
-  &nbsp;
-  Book Total: ${a.price * quantity}
+                {/* <InputLabel id="demo-simple-select-label">Age</InputLabel> */}
+                Quantity &nbsp;
+                <Select
+                  id="demo-simple-select"
+                  value={quantity}
+                  onChange={function (event) {
+                    // here in this function
+                    // i have to set value field above
+                    // to the new changed value
+                    // in event.target.value
+                    setQuantity(event.target.value);
+                  }}
+                  // when i change dropdown value
+                  // onChange event will be triggered
+                  // i have to set value/quantity to this new event's value
+                >
+                  <MenuItem value={1}>1</MenuItem>
+                  <MenuItem value={2}>2</MenuItem>
+                  <MenuItem value={3}>3</MenuItem>
+                  <MenuItem value={4}>4</MenuItem>
+                  <MenuItem value={5}>5</MenuItem>
+                </Select>
+                {/* Quantity selected: {quantity} */}
+                {/* <br /> */}
+                &nbsp; Book Total: ${a.price * quantity}
               </Typography>
             </CardContent>
           </CardActionArea>
           <CardActions>
-          <Button onClick={addToCartClickFun()} size="small" color="primary">
+            <Button onClick={addToCartClickFun} size="small" color="primary">
               Add to cart
               {/* quantity, price, title, imageURL */}
             </Button>
